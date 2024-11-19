9c770bf2b23998f15a467b26267ff01d03d75f13
Mo-Sahil11 <149935356+Mo-Sahil11@users.noreply.github.com>
2024-11-19 19:40:40 +0530

    Add files via upload

---
+++ Employee_Management_System/InvalidSalaryException.java
@@ -0,0 +1,7 @@
+package in.ac.adit.pwj.miniproject.employees.exceptions;
+
+public class InvalidSalaryException extends Exception {
+    public InvalidSalaryException(String message) {
+        super(message);
+    }
+}
